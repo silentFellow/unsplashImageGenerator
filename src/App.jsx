37daed9c6fd038a607de042d43b3fc5648f20cfa
theGreatestIntelligent <thegreatestintelligent@gmail.com>
2023-03-14 10:21:07 +0530

    router module arranged

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,27 @@ const App = () => {
 
   return (
     <BrowserRouter>
-    <div className="h-screen bg-light flex flex-col">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signIn' element={<Nav />} />
-        <Route path='/signUp' element={<Nav />} />
-      </Routes>
-      <Routes>
-        <Route path='/signIn' exact element={<SignIn />} />
-        <Route path='/signUp' element={<SignUp />} />
-      </Routes>
-      
-    </div>
+      <div className="h-screen bg-light flex flex-col">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+        </Routes>
+        <Routes>
+          <Route path='/signIn' element={
+            <>
+              <Nav />
+              <SignIn />
+            </>
+          }/>
+        </Routes>
+        <Routes>
+          <Route path='/signUp' element={
+            <>
+              <Nav />
+              <SignUp />
+            </>
+          }/>
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
